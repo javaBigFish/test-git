4e2db12d1bde40bc52a6f33d4b750cefa23a8b44
yunhua_glb <yunhua_glb@mine.com>
2020-03-20 07:52:31 +0800

    test git commit

---
+++ src/com/yunhua/git/MainDemo.java
@@ -0,0 +1,15 @@
+package com.yunhua.git;
+
+import com.yunhua.git.domain.User;
+
+
+public class MainDemo {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setName("yunhua");
+        user1.setAge("25");
+        System.out.println(user1);
+
+
+    }
+}

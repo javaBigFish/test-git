d87fb3ad0ecd5c69f25b306a3c2f92b0464f24b4
yunhua_glb <yunhua_glb@mine.com>
2020-03-21 08:25:06 +0800

    test ignore file

--- src/com/yunhua/git/MainDemo.java
+++ src/com/yunhua/git/MainDemo.java
@@ -19,7 +19,7 @@ public class MainDemo {
         for (i = 0; i <10 ; i++) {
             System.out.println(i);
         }
-        for (int i = 0; i <10 ; i++) {
+        for (int j = 0; j <10 ; j++) {
             System.out.println(i+"测试忽略文件的问题");
         }
 
